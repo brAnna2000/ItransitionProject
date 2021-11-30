bb8505accd0271efa59528f48c50827f09482acd
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-11-30 22:42:53 +0300

    Add files via upload

---
+++ app/src/pages/index.js
@@ -0,0 +1,13 @@
+// import Authorization from './Authorization';
+// import Courses from './Courses';
+// import CoursesLectures from './CoursesLectures';
+// import MainPage from './MainPage';
+// import UserPage from './UserPage';
+
+// export {
+    // Authorization,
+    // Courses, 
+    // CoursesLectures,
+    // MainPage,
+    // UserPage,
+// };

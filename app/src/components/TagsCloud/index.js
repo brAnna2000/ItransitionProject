8744cffceebdfecf351cdbce58c3ffe8f3b59524
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-04 02:45:15 +0300

    Add files via upload

--- app/src/components/TagsCloud/index.js
+++ app/src/components/TagsCloud/index.js
@@ -1,14 +1,38 @@
 import {Link} from 'react-router-dom';
+import './TagsCloud.css';
 
 function TagsCloud(props){
     const numbersLink = '/modules';
     const numbersLectures = props.names;
+    const SearchTags=(number)=>{
+      let tag = [];
+      tag.push(number)
+      tag = JSON.stringify(tag)
+      console.log(tag)
+      fetch('/searchtag',{
+        method: 'POST',
+        headers: new Headers({
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }),
+        mode: 'same-origin',
+        body: tag
+      })
+      // .then(res =>{
+      //   if(res.redirected){
+      //     // alert(res.url)
+      //     console.log(res)
+      //     // window.location.href = res.url;
+      //   }
+      // })
+      console.log(number)
+    }
     const Tags = numbersLectures.map((number) =>
-    <div className = 'coursesLectures' key={number.toString()}>
-      <Link to={numbersLink}>{number}</Link>
-    </div>);
+    <span className = 'tagsSpan' key={number.toString()}>
+      <Link to={numbersLink} onClick={()=>{SearchTags(number)}}>#{number}</Link>
+    </span>);
     return (
-      <div>
+      <div className="tags">
         <p>Облако тегов</p>
         <div>
           {Tags}

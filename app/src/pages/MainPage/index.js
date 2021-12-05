ddd8ec92a3c8921430b5e926df17ed44f3ccd4cb
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-05 18:30:49 +0300

    Add files via upload

--- app/src/pages/MainPage/index.js
+++ app/src/pages/MainPage/index.js
@@ -2,47 +2,51 @@ import './MainPage.css';
 import logo from '../../logo.svg';
 import React, {useState, useEffect} from 'react';
 import TagsCloud from '../../components/TagsCloud/index';
+import Stars from '../../components/Stars/index';
+import Loader from '../../components/Loader/index';
+import Search from '../../components/Search/index';
 
 function MainPage(props){ 
   const [data, setData] = useState(null);
+
   useEffect(() => {
     fetch('/mainpage')
     .then(responce=>responce.json())
     .then(responce=>setData(responce))
   },[])
-  // console.log(data)
   let a = '';
   if(data !== null){
     a=data[0]
   }
-    return (
-      <div className="App">
-        {!data ? "Loading..." :
-        <header>
-         <img src={logo} className="App-logo" alt="logo" />
-          <TagsCloud names={a}/>
-          <div className='review'>
-            Самый популярный обзор
-            <span className='reviewName'>{data[1].name}</span>
-            <span>#{data[1].tags}</span>
-            <span>{data[1].group}</span> 
-            <p>{data[1].grade}</p> 
-            <p>{data[1].text}</p>
-            <div>Картинки</div><span>{data[1].likes}</span>
-          </div>
-          <div className='review'>
-            Последний обзор
-            <span className='reviewName'>{data[2].name}</span>
-            <span>#{data[2].tags}</span>
-            <span>{data[2].group}</span> 
-            <p>{data[2].grade}</p> 
-            <p>{data[2].text}</p>
-            <div>Картинки</div><span>{data[2].likes}</span>
-          </div>
-        </header>
-        }
-    </div>
-    );
+console.log(data)
+  return (
+    <div className="App">
+      {!data ? <Loader/> :
+      <header>
+        <TagsCloud names={a}/>
+        <Search reviews={data[1]}/>
+        <p className='reviewP'>Самый популярный обзор</p>
+        <div className='review'>
+          <span className='reviewName'>{data[1].name}</span>
+          {data[1].tags ? <span>#{data[1].tags}</span>: <span></span>}
+          <span>{data[1].group}</span> 
+          <Stars grade = {data[1].grade}/>
+          <p>{data[1].text}</p>
+          <div>Картинки</div><span>{data[1].likes}</span>
+        </div>
+        <p className='reviewP'>Последний обзор</p>
+        <div className='review'>
+          <span className='reviewName'>{data[2].name}</span>
+          {data[2].tags ? <span>#{data[2].tags}</span>: <span></span>}
+          <span>{data[2].group}</span> 
+          <Stars grade = {data[2].grade}/>
+          <p>{data[2].text}</p>
+          <div>Картинки</div><span>{data[2].likes}</span>
+        </div>
+      </header>
+      }
+  </div>
+  );
 }
 
 export default MainPage;

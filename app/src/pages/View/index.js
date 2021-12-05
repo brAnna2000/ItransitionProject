c324febb48c4e15af5c54f00389277f920c366ad
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-05 18:22:25 +0300

    Add files via upload

--- app/src/pages/View/index.js
+++ app/src/pages/View/index.js
@@ -1,1 +1,35 @@
-3
+import './View.css';
+import Stars from '../../components/Stars/index';
+import {React, useState, useEffect} from 'react';
+import Loader from '../../components/Loader/index';
+function View(props){
+
+  // const [data, setData] = useState(null);
+  //   useEffect(() => {
+  //       fetch('/view')
+  //       .then(responce=>responce.json())
+  //       .then(responce=>setData(responce))
+  //     },[])
+  //     console.log(data)
+  //   let View = 'Loading';
+    // if (data !== null){
+    //     View =
+    //     <div className='review'>
+    //       <div className='fusion'>
+    //         <span className='reviewName'>{data.name}</span>
+    //       </div>
+    //       <span>{data.group}</span> 
+    //       <Stars grade={data.grade}/>
+    //       <p className='text'>{data.text}</p>
+    //       <div>Картинки</div><span>{data.likes}</span>
+    //     </div>
+    //   }
+  return (
+      <div>
+        <Loader/>
+        {/* {!data ? <Loader/> : View} */}
+      </div>
+  );
+}
+
+export default View;

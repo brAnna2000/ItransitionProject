f7ec1fb01c07fd79111bc8e3e751b0acadcffe18
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-05 18:21:40 +0300

    Add files via upload

--- app/src/pages/TagPage/index.js
+++ app/src/pages/TagPage/index.js
@@ -1,1 +1,40 @@
-4
+import './TagPage.css';
+import React, {useState, useEffect} from 'react';
+import Stars from '../../components/Stars/index';
+import Loader from '../../components/Loader/index';
+
+function TagPage(props){ 
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/tagpage')
+    .then(responce=>responce.json())
+    .then(responce=>setData(responce))
+  },[])
+console.log(data)
+
+const Reviews = ''
+if (data !== null){
+  Reviews = data.map((review) =>
+  <div className='review' key={data.indexOf(review).toString()}>
+    <span className='reviewName'>{review.name}</span>
+    <span>{review.group}</span> 
+    <span>#{review.tags[0]}</span> 
+    <Stars grade={review.grade}/>
+    <p className='text'>{review.text}</p>
+    <div>Картинки</div><span>{review.likes}</span>
+  </div>
+  );
+}
+  return (
+    <div className="App">
+      {!data ? <Loader/> :
+      <header>
+        {Reviews}
+      </header>
+      }
+  </div>
+  );
+}
+
+export default TagPage;

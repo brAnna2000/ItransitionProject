abe2f4b1008d5d570ed4b8b995789d67f78f48c1
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-05 13:33:49 +0300

    Add files via upload

--- app/src/routes.js
+++ app/src/routes.js
@@ -5,18 +5,20 @@ import UserPage from './pages/UserPage'
 import AuthPage from './pages/AuthPage'
 import CreateReview from './pages/CreateReview'
 import AdminPage from './pages/AdminPage'
-import View from './pages/View'
+// import View from './pages/View'
+import TagPage from './pages/TagPage'
 
 export const useRoutes = isAuthenticated => {
   if (isAuthenticated) {
     return (
       <Routes>
         <Route path="/" element={<MainPage/>} exact></Route>
+        <Route path="/userpage/" element={<UserPage />} exact></Route>
         <Route path="/userpage/:id" element={<UserPage />} exact></Route>
         <Route path="/adminpage" element={<AdminPage />} exact></Route>
         <Route path="/createreview" element={<CreateReview />} exact></Route>
-        <Route path="/view" element={<View />} exact></Route>
-        <Route to="/userpage/:id" />
+        {/* <Route path="/view" element={<View />} exact></Route> */}
+        <Route path="/tagpage" element={<TagPage />} exact></Route>
       </Routes>
     )
   }

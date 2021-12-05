8e6b1f2360bc4c57a32bcd2928e66eafbe686acc
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-05 18:26:15 +0300

    Add files via upload

--- app/src/components/Loader/index.js
+++ app/src/components/Loader/index.js
@@ -1,1 +1,19 @@
-3
+import './Loader.css';
+
+function Loader(props){
+    return (
+        <div className="lds-default">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>    
+    );
+}
+
+export default Loader;

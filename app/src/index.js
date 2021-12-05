ffe62d0f74dd4655d1d0919849521618ab624716
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-05 18:29:27 +0300

    Add files via upload

--- app/src/index.js
+++ app/src/index.js
@@ -2,25 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
-import {
-  // MainPage,
-  // UserPage
-} from './pages';
 
 ReactDOM.render(
-  // <Router>
-  //   <Routes>
-      <App>
-        {/* <Route path='/mainpage' element={<MainPage/>} /> */}
-        {/* <Route path='/userpage' element={<UserPage/>} /> */}
-      </App>,
-      document.getElementById('root')
-    /* </Routes>
-  </Router>, */
-  
+  <App></App>,
+  document.getElementById('root')
 );

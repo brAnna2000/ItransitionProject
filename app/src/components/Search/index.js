84ff43a1d2e4dddccf9cf124eab8cee5307cb379
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-05 18:27:11 +0300

    Add files via upload

--- app/src/components/Search/index.js
+++ app/src/components/Search/index.js
@@ -1,1 +1,19 @@
-23
+import './Search.css';
+
+function Search(props){
+    const reviews = props.reviews;
+    // let SearchObj = [1,2,3,4,5,6,7,8,9,10]
+    // const Search = SearchObj.map((number) =>
+    // <p>number</p>
+    // );
+    return (
+        <div>
+            <div>
+
+            </div>
+          {Search}
+        </div>
+    );
+}
+
+export default Search;

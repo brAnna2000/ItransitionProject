1fce89e9ddb7bdc9ecac8182b09d69ead1f84a5c
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-05 17:27:33 +0300

    Add files via upload

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,13 +1,15 @@
-// import Authorization from './Authorization';
-// import Courses from './Courses';
-// import CoursesLectures from './CoursesLectures';
-// import MainPage from './MainPage';
-// import UserPage from './UserPage';
+import AdminPage from './AdminPage';
+import AuthPage from './AuthPage';
+import CreateReview from './CreateReview';
+import TagPage from './TagPage';
+import MainPage from './MainPage';
+import UserPage from './UserPage';
 
-// export {
-    // Authorization,
-    // Courses, 
-    // CoursesLectures,
-    // MainPage,
-    // UserPage,
-// };
+export {
+    AdminPage,
+    AuthPage,
+    CreateReview, 
+    TagPage,
+    MainPage,
+    UserPage,
+};

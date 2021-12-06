cd4df5cff1d547f62ada86d4e9a482fa89b78920
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-06 17:56:45 +0300

    Add files via upload

--- app/src/pages/View/index.js
+++ app/src/pages/View/index.js
@@ -4,32 +4,32 @@ import {React, useState, useEffect} from 'react';
 import Loader from '../../components/Loader/index';
 function View(props){
 
-  // const [data, setData] = useState(null);
-  //   useEffect(() => {
-  //       fetch('/view')
-  //       .then(responce=>responce.json())
-  //       .then(responce=>setData(responce))
-  //     },[])
-  //     console.log(data)
-  //   let View = 'Loading';
-    // if (data !== null){
-    //     View =
-    //     <div className='review'>
-    //       <div className='fusion'>
-    //         <span className='reviewName'>{data.name}</span>
-    //       </div>
-    //       <span>{data.group}</span> 
-    //       <Stars grade={data.grade}/>
-    //       <p className='text'>{data.text}</p>
-    //       <div>Картинки</div><span>{data.likes}</span>
-    //     </div>
-    //   }
-  return (
-      <div>
-        <Loader/>
-        {/* {!data ? <Loader/> : View} */}
-      </div>
-  );
+const [data, setData] = useState(null);
+useEffect(() => {
+    fetch('/viewpage'+window.location.search)
+    .then(responce=>responce.json())
+    .then(responce=>setData(responce))
+},[])
+
+console.log(data)
+let View = 'Loading';
+if (data !== null){
+  View =
+  <div className='review'>
+    <div className='fusion'>
+      <span className='reviewName'>{data.name}</span>
+    </div>
+    <span>{data.group}</span> 
+    <Stars grade={data.grade}/>
+    <p className='text'>{data.text}</p>
+    <div>Картинки</div><span>{data.likes}</span>
+  </div>
+}
+return (
+  <div>
+    {!data ? <Loader/> : View}
+  </div>
+);
 }
 
 export default View;

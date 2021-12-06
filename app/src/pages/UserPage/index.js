4d35205f75e8d61c57a39d1a53d02e5429402b3f
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-06 17:57:01 +0300

    Add files via upload

--- app/src/pages/UserPage/index.js
+++ app/src/pages/UserPage/index.js
@@ -1,7 +1,9 @@
 import {React, useState, useEffect} from 'react';
 import './UserPage.css';
-import logo from '../../logo.svg';
 import Stars from '../../components/Stars/index';
+import Loader from '../../components/Loader/index';
+import ReviewTags from '../../components/ReviewTags/index';
+import {Link} from 'react-router-dom';
 
 function UserPage(props){ 
   const [data, setData] = useState(null);
@@ -52,7 +54,6 @@ function UserPage(props){
   const Delete=(ev)=>{
     let deleteArr = data.concat();
     let deletePost = data[ev]
-    console.log(deletePost)
     deleteArr.splice(ev,1);
     setData(deleteArr)
     fetch('/userpage/delete',{
@@ -86,6 +87,7 @@ function UserPage(props){
   },[post])
   const View=(ev)=>{
     setPost(data[ev])
+    console.log(post)
   }
 
   const Sort=(field)=>{
@@ -128,42 +130,41 @@ function UserPage(props){
         <span className='reviewName'>{review.name}</span>
         <div className='icons'>
           <i className="fas fa-pen" id={data.indexOf(review).toString()} onClick={(event)=>{Edit(event.target.id)}}></i>
-          <i className="far fa-eye" id={data.indexOf(review).toString()} onClick={(event)=>{View(event.target.id)}}></i>
+          <Link style={{color:'white'}} to={'/viewpage?view='+review.id} ><i className="far fa-eye"></i></Link>
           <i className="fas fa-trash" id={data.indexOf(review).toString()} onClick={(event)=>{Delete(event.target.id)}}></i>
         </div>
       </div>
       <span>{review.group}</span> 
-      {review.tags[0]=="" ? <span></span> : <span>#{review.tags[0]}</span>}
+      {review.tags[0]=="" ? <span></span> : <ReviewTags tags={review.tags}/>}
       <Stars grade={review.grade}/>
-      {flag[data.indexOf(review).toString()] !== true ? <p className='text'>{review.text}</p>: <textarea value={text} onChange={(event)=>{setText(event.target.value)}}></textarea>}
-      <div>Картинки</div><span>{review.likes}</span>
+      {flag[data.indexOf(review).toString()] !== true ? <p className='text'>{review.text}</p>: <textarea id="textarea-review" className="form-control" aria-label="With textarea" value={text} onChange={(event)=>{setText(event.target.value)}}></textarea>}
+      <div className='likes'>{review.likes}<i className="far fa-heart"></i></div>
     </div>
   );
   }
     return (
       <div className="App">
-        {!data ? "Loading..." :
-        <header>
-          <div className='new'>
-            <button onClick={()=>{ButtonClick()}}>Создать обзор</button>
-          </div>
+        {!data ? <Loader/> :
+        <div>
           <div className='filter'>
             <span>Фильровать по:</span>
             <span>Названию</span>
             <span>Группе</span>
             <span>Тегам</span>
-            <input onChange={(event)=>{setSearch(event.target.value)}} value={search}></input>
-            <button onClick={()=>{Filter(search)}}>Найти</button>
+            <div className="input-group mb-3" id='searchButton'>
+              <input id='search-input' type="text" className="form-control" placeholder="Найти обзор" aria-label="Найти обзор" aria-describedby="basic-addon2" onChange={(event)=>{setSearch(event.target.value)}} value={search}/>
+              <span className="input-group-text" id="basic-addon2" onClick={()=>{Filter(search)}}>Поиск</span>
+            </div>
           </div>
           <div className='sort'>
             <span>Сортировать по:</span>
             <span onClick={()=>{Sort('name')}}>Названиям</span>
             <span onClick={()=>{Sort('grade')}}>Оценкам</span>
             <span onClick={()=>{Sort('likes')}}>Лайкам</span>
+            <button id='new'onClick={()=>{ButtonClick()}} type="button" className="btn btn-primary">Создать обзор</button>
           </div>
-          {/* {props.match.params.id}!!! */}
           {Reviews}
-        </header>
+        </div>
         }
     </div>
     );

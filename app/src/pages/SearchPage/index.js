91834eb66716527c4d2039cec7b223a1c204b070
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-06 17:59:19 +0300

    Add files via upload

--- app/src/pages/SearchPage/index.js
+++ app/src/pages/SearchPage/index.js
@@ -1,1 +1,18 @@
+import {React, useState, useEffect} from 'react';
+import Loader from '../../components/Loader/index';
+function SearchPage(props){
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch('/search')
+    .then(responce=>responce.json())
+    .then(responce=>setData(responce))
+  },[])
+  console.log(data)
+return (
+  <div>
+    <Loader/>
+  </div>
+);
+}
 
+export default SearchPage;

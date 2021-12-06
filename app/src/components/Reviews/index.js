c0d631fc11cdc2332ba99d0a6926976f5616dda6
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-06 17:54:52 +0300

    Add files via upload

--- app/src/components/Reviews/index.js
+++ app/src/components/Reviews/index.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import Stars from '../Stars/index';
+import ReviewTags from '../ReviewTags/index';
 
+function Reviews(props){
+    const data = props.data;
+    console.log(data)
+    return (
+    <div className='review'>
+        <span className='reviewName'>{data.name}</span>
+        <span>{data.group}</span> 
+        {data.tags ? <ReviewTags tags={data.tags}/>: <span></span>}
+        <Stars grade={data.grade}/>
+        <p className='text'>{data.text}</p>
+        <div className='likes'>{data.likes}<i className="far fa-heart"></i></div>
+    </div>    
+    );
+}
+
+export default Reviews;

3cbe4d660af694afdf6275f180759ceb4f954028
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-06 17:55:49 +0300

    Add files via upload

--- app/src/components/ReviewTags/index.js
+++ app/src/components/ReviewTags/index.js
@@ -1,1 +1,14 @@
+import {React} from 'react';
 
+function ReviewTags(props){
+    const propsTags = props.tags;
+    const Tags = propsTags.map((el) =>
+    <span key={propsTags.indexOf(el)}>#{el}</span>);
+    return (
+      <div>
+          {Tags}
+      </div>
+    );
+}
+
+export default ReviewTags;

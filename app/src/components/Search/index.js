1a6e5037007012b88a9b7f2263fe0af222c50f08
Anna <64814170+brAnna2000@users.noreply.github.com>
2021-12-06 17:53:08 +0300

    Add files via upload

--- app/src/components/Search/index.js
+++ app/src/components/Search/index.js
@@ -1,19 +1,44 @@
 import './Search.css';
+import React, {useState} from 'react';
 
 function Search(props){
-    const reviews = props.reviews;
-    // let SearchObj = [1,2,3,4,5,6,7,8,9,10]
-    // const Search = SearchObj.map((number) =>
-    // <p>number</p>
-    // );
-    return (
-        <div>
-            <div>
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const reviews = props.reviews;
 
-            </div>
-          {Search}
-        </div>
-    );
+  const Filter=(text)=>{
+    if(!search){
+      return
+    }
+    else{
+      let a = [];
+      a.push(search)
+      fetch('/search',{
+        method: 'POST',
+        headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+        }),
+        mode: 'same-origin',
+        body: JSON.stringify(a)
+      }).then(res =>{
+        console.log(res)
+        if(res.status==200){
+          window.location.href = res.url
+        }
+        else{
+          alert('Обзор не найден')
+        }
+      }
+    )
+    }
+  }
+  return (
+      <div className="input-group mb-3 search" id='search-block'>
+        <input id='search-input' type="text" className="form-control" placeholder="Найти обзор" aria-label="Найти обзор" aria-describedby="basic-addon2" onChange={(event)=>{setSearch(event.target.value)}} value={search}/>
+        <span className="input-group-text" id="basic-addon2" onClick={()=>{Filter(search)}}>Поиск</span>
+      </div>
+  );
 }
 
 export default Search;
